b64bd7f500f3d0209b748ce746834cc634c1e6e2
CodeHup <codehup@gmail.com>
2018-04-19 19:35:20 +0530

    Added commit with bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    thumbnail = models.ImageField(upload_to='thumbnail/')
+    heroimage = models.ImageField(upload_to='heroimage/')
+    meta = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    body= models.TextField()
